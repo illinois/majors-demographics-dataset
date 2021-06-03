c54cc4a1ae7af2e8924978205d294e45146f16d0
Max Fan <root@max.fan>
2021-06-02 21:51:15 -0400

    Cleanup term designations and strip whitespace from major names

--- reproduce.py
+++ reproduce.py
@@ -5,14 +5,17 @@ import math
 
 data = []
 for filename in glob.glob("www.dmi.illinois.edu/stuenr/ethsexres/*.xls"):  # iterate
-    year = filename[-6:-4]
-    term = filename[-8:-6]
     year_data = pd.read_excel(
         filename, skiprows=4, header=[0, 1], na_values=["", np.NaN, "nan", float("nan")]
-    )
+    ).convert_dtypes()
+
+    filename = filename.replace("new", "")
+    year = filename[-6:-4]
+    term = filename[-8:-6]
 
     # Add useful cols
-    year_data["term"] = [term] * len(year_data)
+    term_mapping = {"sp": "spring", "fa": "fall", "su": "summer"}
+    year_data["term"] = [term_mapping[term]] * len(year_data)
     year_data["year"] = [year] * len(year_data)
 
     year_data.columns = year_data.columns.map(  # clean up col names
@@ -41,9 +44,10 @@ df = pd.concat(data).reset_index()  # combine
 df["academic_program_code"] = (
     df["academic_program_code"]
     .astype(str)
-    .apply(lambda x: str(x).rstrip())
+    .apply(lambda x: x.rstrip())
     .apply(lambda x: np.NaN if x == "nan" or not x.rstrip() else x)
 )
+df["major_name"] = df["major_name"].astype(str).apply(lambda x: x.rstrip())
 df = df.dropna(subset=["academic_program_code"]).reset_index()  # drop useless cols
 
 del df["index"]
